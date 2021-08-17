c69e24fadf6d806c63d3fdb4ad317d4288ad6c5c
Edi Wiraya <edyw@hotmail.com>
2021-08-17 23:21:56 +0800

    Fix discord attachment with text not sync to telegram

--- src/event-handlers/event-discord.ts
+++ src/event-handlers/event-discord.ts
@@ -6,44 +6,28 @@ import logger from '../helpers/logger'
 import { isGuildOwner } from '../helpers/discord-object'
 import config from '../config'
 
-
-const _isMention = (arg: any) => {
-	return arg.startsWith('<@') && arg.endsWith('>')
-}
-
-const _getMentionedUserNickname = (message: any, mention: any) => {
-	mention = mention.slice(2, -1);
-	if (mention.startsWith('!')) {
-		mention = mention.slice(1);
-	}
-
-	let member
-	if(!message.guild.members.cache){
-		member = message.guild.members.fetch(mention)
-	}
-	else{
-		member = message.guild.members.cache.get(mention)
-	}
-	return member
-}
-
 const _addEscapeToSpecialCharacter = (messageContent: string) => {
 	messageContent = messageContent.replace(/((\_|\*|\[|\]|\(|\)|\~|\`|\>|\#|\+|\-|\=|\||\{|\}|\.|\!){1})/g, '\\$1')
 	return messageContent
 }
 
-const _getFormattedMessageForTelegram = (message: any) => {
+const _getFormattedMessageForTelegram = (discordMsg: any): string => {
 	const _isAttachment = (messageAttachment: any) => {
 		return messageAttachment.first()
 	}
 
-	let messageContent
-  if (_isAttachment(message.attachments)) {
-		messageContent = message.attachments.first().url
-  } else {
-    messageContent = message.content
+	let messageContent = ''
+  console.log(discordMsg.attachments)
+  console.log(discordMsg.cleanContent)
+
+  if (discordMsg.cleanContent !== undefined)
+    messageContent = discordMsg.cleanContent + '\n'
+
+  if (_isAttachment(discordMsg.attachments)) {
+		messageContent += discordMsg.attachments.first().url
   }
-	return _addEscapeToSpecialCharacter(messageContent)
+ 
+  return _addEscapeToSpecialCharacter(messageContent)
 }
 
 const onInteractionCreate = (interaction: Discord.Interaction) => {
@@ -55,26 +39,26 @@ const onInteractionCreate = (interaction: Discord.Interaction) => {
   }
 }
 
-const onMessageCreate = async (message: Discord.Message) => {
+const onMessageCreate = async (discordMsg: Discord.Message) => {
   try {
-    if (message.content.toLowerCase().startsWith(process.env.DISCORD_COMMAND_PREFIX!)) {
-      const command = message.content.split(/\s+/)[1]
-      const args = message.content.split(/\s+/).slice(2)
+    if (discordMsg.content.toLowerCase().startsWith(process.env.DISCORD_COMMAND_PREFIX!)) {
+      const command = discordMsg.content.split(/\s+/)[1]
+      const args = discordMsg.content.split(/\s+/).slice(2)
       if (!config.discord.commands.has(command)) return
 
-      logger.verbose(`Discord command: [${command}], args: [${args}], owner: ${isGuildOwner(message)}`)
-      config.discord.commands.get(command).execute(message, args)
+      logger.verbose(`Discord command: [${command}], args: [${args}], owner: ${isGuildOwner(discordMsg)}`)
+      config.discord.commands.get(command).execute(discordMsg, args)
       return
     }
 
-    if (!message.author.bot) {
-      const syncRec = config.syncMap.channelId.get(message.channelId)
+    if (!discordMsg.author.bot) {
+      const syncRec = config.syncMap.channelId.get(discordMsg.channelId)
       if (syncRec === undefined) return
 
-      const messageContent = _getFormattedMessageForTelegram(message)
-      const username = _addEscapeToSpecialCharacter(message.member!.displayName)
+      const messageContent = _getFormattedMessageForTelegram(discordMsg)
+      const username = _addEscapeToSpecialCharacter(discordMsg.member!.displayName)
       const msg = `*${username}*: ${messageContent}`
-      messaging.telegramSendMessage(syncRec.telegram.groupId, msg) // TODO:
+      messaging.telegramSendMessage(syncRec.telegram.groupId, msg) 
       logger.verbose(`[Message] Discord(${syncRec.discord.guildName}/${syncRec.discord.channelName}) -> Telegram(${syncRec.telegram.groupName}): ${msg}`)
     }
   } catch (e) {
